ba1104207a845457186b45c441c0c51d8727be96
SerenaMK <serenemudekoko@gmail.com>
2022-10-25 17:50:32 +0200

    Esercizio completato.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
+// Quando faccio click sul button....
+
 document.getElementById("button").addEventListener("click", function () {
 
+    // Resetta tutto
     reset();
 
     var oggetti = 0;
     var budget = document.getElementById("customBudget").valueAsNumber;
     var limite = document.getElementById("customLimit").valueAsNumber;
 
+
+    // Se i campi sono vuoti, mostra l'avviso, altrimenti rimuovi l'avviso e procedi
     if (!budget || !limite) {
         document.getElementById("warning").innerHTML = "Attenzione! Compilare entrambi i campi.";
         return;
@@ -13,9 +18,12 @@ document.getElementById("button").addEventListener("click", function () {
         document.getElementById("warning").innerHTML = "";
     }
 
+
+    // Mostra quali sono il budget ed il limite
     document.getElementById("budget").innerHTML = budget;
     document.getElementById("limit").innerHTML = limite;
 
+    // Ciclo while. Continua a generare un valore random ed a sottrarlo al budget, poi stampa il risultato sulla pagina... ma solo se il valore di spesa non supera quello del budget!
     while (budget > limite) {
 
         var spesa = Math.floor(Math.random() * 100);
@@ -29,12 +37,15 @@ document.getElementById("button").addEventListener("click", function () {
         
     }
 
+    // Concludi stampando quanti oggetti sono stati acquistati ed avvisando che è stato superato il valore limite.
     document.getElementById("msgAcquistato").innerHTML = `<li>Hai acquistato <span>${oggetti}</span> oggetti.</li>`;
 
     document.getElementById("msgTroppo").innerHTML = `<li>Hai meno di <span>${limite}</span> &euro;. Ora basta! Stai spendendo troppo!</li>`;
 
 })
 
+
+// Funzione di reset
 function reset() {
     budget = "";
     limite = "";
